ee2aeefd273867e54f8cae56ffc93ebcd2e36efc
lukeify <5379845+lukeify@users.noreply.github.com>
2024-10-07 13:22:05 +1300

    Initial commit

---
+++ diveboard-cli/DiveboardCliTool.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  diveboard-cli
+//
+//  Created by Luke on 7/10/24.
+//
+
+import ArgumentParser
+
+@main
+struct DiveboardCliTool: ParsableCommand {   
+    public func run() throws {
+        print("Test")
+    }
+}
