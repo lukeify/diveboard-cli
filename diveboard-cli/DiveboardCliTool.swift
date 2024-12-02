f8b04f2ed87fdd3b6ad26e1e6e3d789228689dfc
lukeify <5379845+lukeify@users.noreply.github.com>
2024-12-02 15:51:51 +1300

    refactor(arguments): validate correct options are passed for apply subcommand

--- diveboard-cli/DiveboardCliTool.swift
+++ diveboard-cli/DiveboardCliTool.swift
@@ -10,28 +10,8 @@ import ArgumentParser
 @main
 struct DiveboardCliTool: ParsableCommand {
     static var configuration = CommandConfiguration(
+        subcommands: [ApplyCommand.self]
         )
-    
-    @Argument(help: "The command to run using diveboard.")
-    var command: DiveboardCommand
-    
-    @Option(name: .long, help: "The location to retrieve the divefiles repository from.")
-    var source: DivefileRepositorySource = .github
-    
-    @Option(name: .long, help: "The GitHub account to use for a divefile repository.")
-    var githubAccountIdentifier: String
-    
-    @Option(name: .long, help: "The directory to use as the divefile repository.")
-    var localDirectoryPath: String
-    
-    public func run() throws {
-        switch source {
-        case .github:
-            GitHubDivefilesRetrieval.retrieve(identifier: githubAccountIdentifier)
-        case .local:
-            LocalDirectoryRetrieval.retrieve(path: localDirectoryPath)
-        }
-    }
 }
 
 enum DivefileRepositorySource: String, ExpressibleByArgument {
@@ -39,6 +19,39 @@ enum DivefileRepositorySource: String, ExpressibleByArgument {
     case local = "local"
 }
 
-enum DiveboardCommand: String, ExpressibleByArgument {
-    case apply = "apply"
+struct ApplyCommand: ParsableCommand {
+    static var configuration = CommandConfiguration(commandName: "apply")
+        
+    @Option(name: .long, help: "The location to retrieve the divefiles repository from.")
+    var source: DivefileRepositorySource = .github
+    
+    @Option(name: .long, help: "The GitHub account to use for a divefile repository.")
+    var githubAccountIdentifier: String?
+    
+    @Option(name: .long, help: "The directory to use as the divefile repository.")
+    var localDirectoryPath: String?
+    
+    /// Validates for the given `source`, the correct dependent options are present.
+    mutating public func validate() throws {
+        switch source {
+        case .local:
+            if localDirectoryPath == nil {
+                throw ValidationError("Please specify a directory for the divefile repository using --local-directory-path.")
+            }
+        case .github:
+            if githubAccountIdentifier == nil {
+                throw ValidationError("Please specify a GitHub account name to retrieve the `divefiles` repoistory from using --github-account-identifier.")
+            }
+        }
+    }
+    
+    public func run() throws {
+        switch source {
+        case .github:
+            GitHubDivefilesRetrieval.retrieve(identifier: githubAccountIdentifier!)
+        case .local:
+            LocalDirectoryRetrieval.retrieve(path: localDirectoryPath!)
+        }
+    }
+    
 }
